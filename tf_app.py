45351924e03386927d32f1cf8101d71825473197
Logan McNaughton <logan@bacoosta.com>
2020-07-22 13:36:26 -0600

    use bash

--- tf_app.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import os
-import pathlib
-import git
-
-if not os.path.exists('models'):
-    git.Repo.clone_from('https://github.com/tensorflow/models.git', 'models')
-g = git.Git('models')
-g.pull('origin','master')
